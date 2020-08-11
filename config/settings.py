d94e7cfae0343a4bc44616af825983f6f3a4354c
Flaviu Cojocariu <cojokka@gmail.com>
2020-08-11 14:14:06 +0200

    removed sendgrid references from code

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,3 @@
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SG_KEY = os.environ.get('SG_KEY')
--- routes/main.py
+++ routes/main.py
@@ -1,15 +1,12 @@
-from config.settings import SG_KEY
+import smtplib
+
 from config.utils import request_limit
 from config.utils import allowed_contact_today
-
-import smtplib, ssl
 
 from flask import Blueprint, request, escape, abort
 from datetime import timedelta
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
-# from sendgrid import SendGridAPIClient
-# from sendgrid.helpers.mail import Mail
 
 test_schema = {
   'type': 'object',
@@ -45,37 +42,29 @@ def main_route():
 @request_limit(10, timedelta(minutes=1))
 def contact():
   port = 465
-  context = ssl.create_default_context()
-  with smtplib.SMTP_SSL('localhost', port, context) as server:
-    message = """\
-      Subject: Hi there
+  r_data = request.data
+  if len(r_data) == 0:
+    return 'Not a valid body', 400
+  r_data = request.json
+
+  try:
+    validate(instance=r_data, schema=test_schema)
+    name: str = escape(r_data['name'])
+    email: str = escape(r_data['email'])
+    if not allowed_contact_today(email, name):
+      return abort(429)
+    message_body: str = escape(r_data['message'])
+    message = f"""\
+      Subject: Hi Mailtrap
+      To: {receiver}
+      From: {sender}
       
-      This message is sent from Python."""
-    server.sendmail('test@api.flaviuscojocariu.com', 'cojokka@gmail.com', message)
-  # r_data = request.data
-  # if len(r_data) == 0:
-  #   return 'Not a valid body', 400
-  # r_data = request.json
-  # try:
-  #   validate(instance=r_data, schema=test_schema)
-  #   name: str = escape(r_data['name'])
-  #   email: str = escape(r_data['email'])
-  #   if not allowed_contact_today(email, name):
-  #     return abort(429)
-  #   message_body: str = escape(r_data['message'])
-  #   message = Mail(
-  #     from_email= 'cojokka@gmail.com',
-  #     to_emails='cojokka@gmail.com',
-  #     subject='Contact from Personal-Website <flaviuscojocariu.com>',
-  #     html_content= '''
-  #       <p>A message from the personal website.</p>
-  #       <p>Name: {}</p>
-  #       <p>Email: {}</p>
-  #       <p>Message: {}</p>
-  #     '''.format(name, email, message_body))
-  # except ValidationError as e:
-  #   return { 'message': e.schema['message'], 'data': list(e.path) }, 400
-  # sg = SendGridAPIClient(SG_KEY)
-  # response = sg.send(message)
+      {body}"""
+
+    with smtplib.SMTP('localhost', port) as server:
+      server.sendmail('test@api.flaviuscojocariu.com', 'cojokka@gmail.com', message(receiver=email, sender=email,
+                                                                                  body=message_body))
+  except ValidationError as e:
+    return { 'message': e.schema['message'], 'data': list(e.path) }, 400
   # print(response.status_code, response.body, response.headers)
   return 'sent email', 200
